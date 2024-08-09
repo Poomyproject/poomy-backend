1255a9039c1b4269b9c4099ec1ba34b23808ea6d
안나영 <dpswpfskdud@naver.com>
2024-08-09 21:09:57 +0900

    feat: home에 필요한 entity,controller 생성

---
+++ src/main/java/com/poomy/mainserver/favorite/entity/Favorite.java
@@ -0,0 +1,27 @@
+package com.poomy.mainserver.favorite.entity;
+
+import com.poomy.mainserver.home.entity.Shop;
+import com.poomy.mainserver.user.entity.User;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "user_favorite_poom_shop")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Favorite {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "shop_id")
+    private Shop shop;
+}
