c15a66838c62babb2f18141a0770b26b10089fd4
Seohyeon <drif0023@gmail.com>
2024-10-26 01:23:24 +0900

    fix: 뉴스레터, 검색, 찜 조회에서 소품샵 경도, 위도 빼고 위치 나오게 수정

--- src/main/java/com/poomy/mainserver/search/dto/SearchShopResDto.java
+++ src/main/java/com/poomy/mainserver/search/dto/SearchShopResDto.java
@@ -4,15 +4,14 @@ import com.poomy.mainserver.home.entity.Shop;
 import lombok.Builder;
 
 @Builder
-public record SearchShopResDto(Long id, String name, String spot, String mood, Double latitude, Double longitude, String image, Boolean isFavorite) {
+public record SearchShopResDto(Long id, String name, String spot, String mood, String location, String image, Boolean isFavorite) {
     public static SearchShopResDto ofSearchShopByName(Shop shop, String image, Boolean isFavorite){
         return SearchShopResDto.builder()
                 .id(shop.getId())
                 .name(shop.getName())
                 .spot(shop.getSpot().getName())
                 .mood(shop.getMood().getName())
-                .latitude(shop.getLatitude())
-                .longitude(shop.getLongitude())
+                .location(shop.getLocation())
                 .image(image)
                 .isFavorite(isFavorite)
                 .build();
